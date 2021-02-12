0f08d4ca12177eee80c9931d970d01ea6157583f
fr13n8 <ovsepanrik@gmail.com>
2021-02-13 01:56:52 +0400

    Search route functionality

--- src/script.ts
+++ src/script.ts
@@ -29,6 +29,8 @@ const BULDOZERS: Array<CELL> = []
 
 let cell1: [number, number] = null
 let cell2: [number, number] = null
+let paths: Array<Array<null | boolean | number>> = []
+let route: Array<[number, number]> = []
 
 for (let i: number = 0; i < BULDOZER_COUNTS; i++) {
     BULDOZERS.push(
@@ -164,6 +166,94 @@ const createMouse = (element: HTMLCanvasElement) => {
 
 const mouse: MOUSE = createMouse(canvas)
 
+const getPath = (matrix: Array < Array < boolean >>, [x1, y1]: [number, number], [x2, y2] : [number, number]): Array<Array<null | boolean | number>> => {
+    const paths: Array<Array<null | boolean | number>> = []
+
+    for (let y: number = 0; y < matrix.length; y++) {
+        const row: Array<null | boolean> = []
+        for (let x: number = 0; x < matrix[y].length; x++) {
+            row.push(matrix[y][x] === false ? false : null)
+        }
+
+        paths.push(row)
+    }
+
+    paths[y2][x2] = 0
+
+    while(paths[y1][x1] === null){
+
+        for (let y: number = 0; y < matrix.length; y++) {
+            for (let x: number = 0; x < matrix[y].length; x++) {
+                if(paths[y][x] === false || paths[y][x] === null) {
+                    continue
+                }
+
+                const count: number = +paths[y][x] + 1
+
+                if(y > 0 && paths[y - 1][x] !== false) {
+                    if(paths[y - 1][x] !== null) {
+                        paths[y - 1][x] = Math.min(+paths[y - 1][x], count)
+                    } else {
+                        paths[y - 1][x] = count
+                    }
+                }
+                if(y < matrix.length - 1 && paths[y + 1][x] !== false) {
+                    if(paths[y + 1][x] !== null) {
+                        paths[y + 1][x] = Math.min(+paths[y + 1][x], count)
+                    } else {
+                        paths[y + 1][x] = count
+                    }
+                }
+                if(x < matrix[0].length - 1 && paths[y][x + 1] !== false) {
+                    if(paths[y][x + 1] !== null) {
+                        paths[y][x + 1] = Math.min(+paths[y][x + 1], count)
+                    } else {
+                        paths[y][x + 1] = count
+                    }
+                }
+                if(x > 0 && paths[y][x - 1] !== false) {
+                    if(paths[y][x - 1] !== null) {
+                        paths[y][x - 1] = Math.min(+paths[y][x - 1], count)
+                    } else {
+                        paths[y][x - 1] = count
+                    }
+                }
+            }
+        }
+    }
+
+    // let [x2, y2]: [number, number] = [y1, x1]
+        let endRouteValue: number = +paths[y1][x1] 
+        route = [[y1, x1]]
+
+        while(endRouteValue !== 0) {
+            endRouteValue--
+            console.log(y1, x1, endRouteValue)
+            if(y1 > 0 && paths[y1 - 1][x1] === endRouteValue) {
+                route.push([y1 - 1, x1])
+                y1--
+                continue
+            }
+            if(y1 < ROWS_COUNT - 1 && paths[y1 + 1][x1] === endRouteValue) {
+                route.push([y1 + 1, x1])
+                y1++
+                continue
+            }
+            if(x1 < COLUMNS_COUNT - 1 && paths[y1][x1 + 1] === endRouteValue) {
+                route.push([y2, x1 + 1])
+                x1++
+                continue
+            }
+            if(x1 > 0 && paths[y1][x1 - 1] === endRouteValue) {
+                route.push([y1, x1 - 1])
+                x1--
+                continue
+            }
+        }
+    // console.log(paths)
+    return paths
+}
+
 const tick = (): void => {
     requestAnimationFrame(tick)
     
@@ -185,6 +275,41 @@ const tick = (): void => {
             cell1 = [x, y]
             console.log(cell1, cell2)
         }
+
+        if(cell1 && cell2) {
+            paths = getPath(matrix, cell1, cell2)
+        }
+
+        if(paths) {
+            for(let y: number = 0; y < paths.length; y++) {
+                paths[y].map((item: boolean | null | number, x: number) => {
+                    
+                    if( item !== null && item !== false) {
+                        context.fillStyle = "red"
+                        context.font = "30px serif"
+                        context.textAlign = "center"
+                        context.textBaseline = "middle"
+                        context.fillText(
+                            paths[y][x].toString(),
+                            PADDING + x * CELL_SIZE + CELL_SIZE * 0.5,
+                            PADDING + y * CELL_SIZE + CELL_SIZE * 0.5
+                        )
+                    }
+                })
+            }
+        }
+        console.log(paths)
+        console.log(route)
+        // route.map(([y, x]: [number, number]) => {
+        //     context.beginPath()
+        //     context.rect(
+        //         PADDING + x * CELL_SIZE, 
+        //         PADDING + y * CELL_SIZE,
+        //         CELL_SIZE, CELL_SIZE)
+        //     context.fillStyle = "blue"
+        //     context.fill()
+        // })
+
     }
 
     mouse.update()
@@ -196,6 +321,7 @@ const isComlpeted = (): boolean => {
             if (!matrix[y][x]) return false
         }
     }
+
     return true
 }
 
